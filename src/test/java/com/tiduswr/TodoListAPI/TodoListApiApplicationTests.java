ca09297eb59e900eee44893d6aeb33c05dba6ced
Harllem Nascimento <harllemnascimento@gmail.com>
2023-04-10 21:37:43 -0300

    Importação do Projeto via Spring Initializr

---
+++ src/test/java/com/tiduswr/TodoListAPI/TodoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiduswr.TodoListAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
